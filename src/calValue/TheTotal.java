0836d9282b8e67c593eceea9fc22202c8f08199e
testwind <31543711+testwind@users.noreply.github.com>
2018-03-07 18:20:28 +0800

    3-7清理

--- src/calValue/TheTotal.java
+++ src/calValue/TheTotal.java
@@ -1,5 +1,30 @@
 package calValue;
 
+
+
+/**
+ * @startuml
+ *  
+ * digraph G {
+    aize ="4,4";
+    main [shape=box];
+    main -> parse [weight=8];
+    parse -> execute;
+    main -> init [style=dotted];
+    main -> cleanup;
+    execute -> { make_string; printf}
+    init -> make_string;
+    edge [color=red];
+    main -> printf [style=bold,label="100 times"];
+    make_string [label="make a string"];
+    node [shape=box,style=filled,color=".7 .3 1.0"];
+    execute -> compare;
+  }
+ * @enduml
+ * 
+ */
+
+
 public class TheTotal {
 
     // 1-1098按设置固定天（ 多期、一期），
